e4ea49797669b16398b733d36b3a2125eed31a0d
anickacodes <anickalewis@pursuit.org>
2023-05-30 10:31:29 -0400

    updated gitignore

--- index.js
+++ index.js
@@ -3,4 +3,9 @@
  * package management - npm install, etc installs latest updates for you 
  * 
  * node_modules folder can be stored in git ignore
+ * before you commit anything, type gitstaus to see what will be committed 
+ * 
+ * git reset - remove staged files, unneccesary 
+ * 
+ * 
  */
